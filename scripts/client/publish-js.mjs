36c373984e57c726963f00468e27164f6017fddc
Loris Leiva <loris.leiva@gmail.com>
2024-07-24 11:24:55 +0100

    wip

--- scripts/client/publish-js.mjs
+++ scripts/client/publish-js.mjs
@@ -8,17 +8,29 @@ if (!level) {
   throw new Error('A version level — e.g. "path" — must be provided.');
 }
 
+// Go to the client directory and install the dependencies.
 cd(path.join(workingDirectory, 'clients', 'js'));
 await $`pnpm install`;
 
-if (level.startsWith('pre')) {
-  await $`pnpm version ${level} --preid ${tag} --no-git-tag-version`;
-} else {
-  await $`pnpm version ${level} --no-git-tag-version`;
-}
+// Update the version.
+const versionArgs = [
+  '--no-git-tag-version',
+  ...(level.startsWith('pre') ? [`--preid ${tag}`] : []),
+];
+let { stdout } = await $`pnpm version ${level} ${versionArgs}`;
+const newVersion = stdout.slice(1).trim();
 
-if (process.env.CI) {
-  await $`echo "new_version=$(pnpm pkg get version | sed 's/"//g')" >> $GITHUB_OUTPUT`;
-}
+// if (process.env.CI) {
+//   await $`echo "new_version=$(pnpm pkg get version | sed 's/"//g')" >> $GITHUB_OUTPUT`;
+// }
 
-// await $`pnpm publish --no-git-checks --tag ${tag}`;
+// Publish the package.
+await $`pnpm publish --no-git-checks --tag ${tag}`;
+
+// Commit the new version.
+const commitMessage = `Publish JS client v${newVersion}`;
+await $`git commit -am "${commitMessage}"`;
+
+// Tag the new version.
+const tagMessage = `js@v${newVersion}`;
+await $`git tag ${tagMessage}`;
